5da68c4e1cba85bef7cc85ae50a525a9ae83eb51
Marcel Ebert <mail@marcel-ebert.de>
2024-06-27 09:56:12 +0200

    Fix coingecko client

--- dia-batching-server/src/api/coingecko.rs
+++ dia-batching-server/src/api/coingecko.rs
@@ -4,7 +4,6 @@ use crate::AssetSpecifier;
 use serde::de::DeserializeOwned;
 use clap::Parser;
 use std::collections::HashMap;
-use std::convert::TryInto;
 
 #[derive(Parser, Debug, Clone)]
 struct ServiceConfig {
@@ -13,7 +12,7 @@ struct ServiceConfig {
     pub cg_api_key: String,
 
     /// Logging output format.
-    #[clap(long, env = "CG_HOST_URL", default_value = "https://api.coingecko.com/api/v3")]
+    #[clap(long, env = "CG_HOST_URL", default_value = "https://pro-api.coingecko.com/api/v3")]
     pub cg_host_url: String,
 }
 
@@ -36,7 +35,6 @@ impl CoingeckoPriceApi {
     }
 }
 
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -48,7 +46,13 @@ pub struct SimplePing {
 //  /simple/price and /simple/token_price/{id}
 // ---------------------------------------------
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Price {}
+pub struct Price {
+    pub usd: Option<f64>,
+    pub usd_market_cap: Option<f64>,
+    pub usd_24h_vol: Option<f64>,
+    pub usd_24h_change: Option<f64>,
+    pub last_updated_at: Option<u64>,
+}
 
 /// CoinGecko client
 pub struct CoinGeckoClient {
@@ -56,34 +60,54 @@ pub struct CoinGeckoClient {
     api_key: String,
 }
 
+#[derive(Debug)]
+struct CoinGeckoClientError(String);
 
 impl CoinGeckoClient {
     pub fn new(host: String, api_key: String) -> Self {
         CoinGeckoClient { host, api_key }
     }
 
-    async fn get<R: DeserializeOwned>(&self, endpoint: &str) -> Result<R, reqwest::Error> {
+    async fn get<R: DeserializeOwned>(&self, endpoint: &str) -> Result<R, CoinGeckoClientError> {
         let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert("accept", reqwest::header::HeaderValue::from_static("application/json"));
 
-        // We supply both API keys because one of them will work
+        // We supply a different header for the demo API
         let mut api_key = reqwest::header::HeaderValue::from_str(self.api_key.as_str()).expect("Could not create header value");
         api_key.set_sensitive(true);
-        headers.insert("x-cg-demo-api-key", api_key.clone());
-        headers.insert("x-cg-pro-api-key", api_key);
+        if self.host.contains("pro-api") {
+            headers.insert("x-cg-pro-api-key", api_key);
+        } else {
+            headers.insert("x-cg-demo-api-key", api_key);
+        }
 
-        let client = reqwest::Client::builder().default_headers(headers).build()?;
+        let client = reqwest::Client::builder().default_headers(headers).build().map_err(|e| CoinGeckoClientError(e.to_string()))?;
 
         let url = reqwest::Url::parse(format!("{host}/{ep}", host = self.host.as_str(), ep = endpoint).as_str()).expect("Invalid URL");
 
-        client.get(url)
+        let response = client.get(url)
             .send()
-            .await?
-            .json()
-            .await
+            .await;
+
+        match response {
+            Ok(response) => {
+                if !response.status().is_success() {
+                    // return Err(reqwest::Error::from_str("Request failed"));
+                    let result = response.text().await;
+                    Err(CoinGeckoClientError(result.unwrap()))
+                } else {
+                    let result = response.json().await;
+                    result.map_err(|e| CoinGeckoClientError(e.to_string()))
+                }
+            }
+            Err(e) => {
+                Err(CoinGeckoClientError(e.to_string()))
+            }
+        }
     }
 
     /// Check API server status
-    pub async fn ping(&self) -> Result<SimplePing, reqwest::Error> {
+    pub async fn ping(&self) -> Result<SimplePing, CoinGeckoClientError> {
         self.get("/ping").await
     }
 
@@ -100,18 +124,44 @@ impl CoinGeckoClient {
     ///     client.price(&["bitcoin", "ethereum"], &["usd"], true, true, true, true).await;
     /// }
     /// ```
-    pub async fn price<Id: AsRef<str>, Curr: AsRef<str>>(
+    pub async fn price<Id: AsRef<str>>(
         &self,
         ids: &[Id],
-        vs_currencies: &[Curr],
         include_market_cap: bool,
         include_24hr_vol: bool,
         include_24hr_change: bool,
         include_last_updated_at: bool,
-    ) -> Result<HashMap<String, Price>, reqwest::Error> {
+    ) -> Result<HashMap<String, Price>, CoinGeckoClientError> {
         let ids = ids.iter().map(AsRef::as_ref).collect::<Vec<_>>();
+        // We always query for USD
+        let vs_currencies = vec!["usd"];
         let vs_currencies = vs_currencies.iter().map(AsRef::as_ref).collect::<Vec<_>>();
-        let req = format!("/simple/price?ids={}&vs_currencies={}&include_market_cap={}&include_24hr_vol={}&include_24hr_change={}&include_last_updated_at={}", ids.join("%2C"), vs_currencies.join("%2C"), include_market_cap, include_24hr_vol, include_24hr_change, include_last_updated_at);
+        // We always query for full precision
+        let precision = "full";
+        let req = format!("/simple/price?ids={}&vs_currencies={}&precision={}&include_market_cap={}&include_24hr_vol={}&include_24hr_change={}&include_last_updated_at={}", ids.join("%2C"), vs_currencies.join("%2C"), precision, include_market_cap, include_24hr_vol, include_24hr_change, include_last_updated_at);
+        // TODO maybe use json::Value instead of Price for arbitrary values
         self.get(&req).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_ping() {
+        let client = CoinGeckoClient::new("https://api.coingecko.com/api/v3".to_string(), "CG-KX1Xs7FcZKAiEN22SXTWRjZx".to_string());
+        let ping = client.ping().await.expect("Should return a ping");
+        assert_eq!(ping.gecko_says, "(V3) To the Moon!");
+    }
+
+    #[tokio::test]
+    async fn test_price() {
+        let client = CoinGeckoClient::new("https://api.coingecko.com/api/v3".to_string(), "CG-KX1Xs7FcZKAiEN22SXTWRjZx".to_string());
+        let price = client.price(&["stellar"], true, true, true, true).await.expect("Should return a price");
+        assert_eq!(price.len(), 1);
+    }
+}
+
+// 0.088943079510600376
+// 0.08894307951060038
