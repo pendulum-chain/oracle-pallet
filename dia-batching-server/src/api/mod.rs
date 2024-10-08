fa1860c48a8bc544abbed06b42ddac17e3299056
Marcel Ebert <mail@marcel-ebert.de>
2024-10-08 16:00:46 +0200

    Make sure that custom price is returned for USD-ARS

--- dia-batching-server/src/api/mod.rs
+++ dia-batching-server/src/api/mod.rs
@@ -42,20 +42,7 @@ impl PriceApi for PriceApiImpl {
 	async fn get_quotations(&self, assets: Vec<&AssetSpecifier>) -> Vec<Quotation> {
 		let mut quotations = Vec::new();
 
-		// First, get fiat quotations
-		let fiat_assets: Vec<_> = assets
-			.clone()
-			.into_iter()
-			.filter(|asset| PolygonPriceApi::is_supported(asset))
-			.collect();
-
-		let fiat_quotes = self.get_fiat_quotations(fiat_assets.clone()).await;
-		match fiat_quotes {
-			Ok(fiat_quotes) => quotations.extend(fiat_quotes),
-			Err(e) => log::error!("Error getting fiat quotations: {}", e),
-		}
-
-		// Then, get quotations for custom assets
+		// Get supported custom assets
 		let custom_assets: Vec<&AssetSpecifier> = assets
 			.clone()
 			.into_iter()
@@ -68,7 +55,24 @@ impl PriceApi for PriceApiImpl {
 			Err(e) => log::error!("Error getting custom quotations: {}", e),
 		}
 
-		// Finally, get supported crypto quotations
+		// Remove custom assets from the list of assets. This is important because it could happen that
+		// a custom asset is also supported by the Polygon or Coingecko API. In this case, we want to
+		// use the custom asset and not the other API.
+		let assets: Vec<&AssetSpecifier> =
+			assets.into_iter().filter(|asset| !custom_assets.contains(&asset)).collect();
+
+		let fiat_assets: Vec<_> = assets
+			.clone()
+			.into_iter()
+			.filter(|asset| PolygonPriceApi::is_supported(asset))
+			.collect();
+
+		let fiat_quotes = self.get_fiat_quotations(fiat_assets.clone()).await;
+		match fiat_quotes {
+			Ok(fiat_quotes) => quotations.extend(fiat_quotes),
+			Err(e) => log::error!("Error getting fiat quotations: {}", e),
+		}
+
 		let crypto_assets = assets
 			.into_iter()
 			.filter(|asset| CoingeckoPriceApi::is_supported(asset))
